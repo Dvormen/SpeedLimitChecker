68bd46eb595714760a28b78498e96fda92ffcdb7
Dvormen <dvorak13@spsejecna.cz>
2025-12-04 20:15:21 +0100

    Added new class Logger for logging

---
+++ Logger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeedLimitChecker
+{
+    internal class Logger
+    {
+        /// <summary>
+        /// This method writes into Log.txt
+        /// </summary>
+        /// <param name="info"> Number of lines that the program has written </param>
+        /// <param name="file"> File name </param>
+        public void logWrite(int info, string file) 
+        {
+            File.AppendAllText("Log.txt","[" + DateTime.Now + "] Written into file " + file + " " + info + " lines");
+        }
+
+    }
+}
